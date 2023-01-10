747a4af193df0e7653d50504f67f9215be7a0479
Honey171 <103601089+Honey171@users.noreply.github.com>
2023-01-10 18:37:32 +0300

    Create Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Footer from '../components/Footer';
+import Input from '../components/Input';
+import Navbar from '../components/Navbar';
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+      <Input />
+      <Footer />
+    </div>
+  );
+}
